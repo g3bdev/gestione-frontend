3068e548a439e9031af3b9c86af14b169f097167
gab <gabriel.andreata@marconirovereto.it>
2023-02-16 22:28:23 +0100

    basic validators and better styling

--- src/app/add-activity/add-activity.component.ts
+++ src/app/add-activity/add-activity.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, Validators} from "@angular/forms";
 import {DataService} from "../data.service";
+import {data} from "autoprefixer";
 
 @Component({
   selector: 'app-add-activity',
@@ -14,39 +15,50 @@ export class AddActivityComponent implements OnInit {
   }
 
   clients = [];
+  sites = [];
+  intervention_types = ['installation', 'scheduled_maintenance', 'extraordinary_maintenance', 'remote_support', 'programming', 'training', 'planning', 'assembly', 'testing', 'other'];
+  intervention_locations = ['ITW', 'client', 'remote'];
+  work = [];
+  operators = [];
 
-  onSelect(event: Event) {
+  select(event: Event, value: string) {
     this.newActivityForm.patchValue({
-      client: (event.target as HTMLInputElement).value
+      [value]: (event.target as HTMLInputElement).value
     })
   }
 
   newActivityForm = this.formBuilder.group({
     date: ['', Validators.required],
-    intervention_duration: ['', Validators.required],
-    intervention_type: ['', Validators.required],
-    intervention_location: ['', Validators.required],
+    intervention_duration: ['', [Validators.pattern(/^(0?[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/), Validators.required]],
+    intervention_type: ['installation', Validators.required],
+    intervention_location: ['ITW', Validators.required],
     client: ['ITW', Validators.required],
-    site: ['', Validators.required],
-    description: ['', Validators.required],
-    notes: ['', Validators.required],
-    trip_kms: ['', Validators.required],
-    cost: ['', Validators.required],
-    operator_id: ['', Validators.required],
+    site: ['AA01 0222 - Novotic Cloud R_D BM124 1122 Implementaz', Validators.required],
+    description: [''],
+    notes: [''],
+    trip_kms: ['0'],
+    cost: ['0'],
+    operator_id: [''],
   });
 
   submitForm() {
-    this.dataService.sendActivity(this.newActivityForm.value).subscribe((data: any) => {
+    this.dataService.sendActivity(this.newActivityForm.value).subscribe(data => {
       console.log(data);
     });
   }
-
 
   ngOnInit(): void {
     this.dataService.getClients().subscribe((data: any) => {
       console.log(data);
       this.clients = data;
     });
+    this.dataService.getSites().subscribe((data: any) => {
+      console.log(data);
+      this.sites = data;
+    });
+    this.dataService.getWorkTable().subscribe((data: any) => {
+      console.log(data);
+      this.work = data;
+    });
   }
-
 }
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -11,19 +11,27 @@ export class DataService {
   }
 
   getUserById(id: number) {
-    return this.httpClient.get(`http://localhost:8000/users/${id}?application_name=angular`);
+    return this.httpClient.get(`http://localhost:8000/users/${id}`);
   }
 
   getUsers() {
-    return this.httpClient.get(`http://localhost:8000/users?application_name=angular`);
+    return this.httpClient.get(`http://localhost:8000/users`);
   }
 
   getClients() {
-    return this.httpClient.get(`http://localhost:8000/clients?application_name=angular`);
+    return this.httpClient.get(`http://localhost:8000/clients`);
+  }
+
+  getSites() {
+    return this.httpClient.get(`http://localhost:8000/sites`);
+  }
+
+  getWorkTable() {
+    return this.httpClient.get(`http://localhost:8000/work`);
   }
 
   sendActivity(activity: Partial<{ date: string | null; intervention_duration: string | null; intervention_type: string | null; intervention_location: string | null; client: string | null; site: string | null; description: string | null; notes: string | null; trip_kms: string | null; cost: string | null; operator_id: string | null; }>) {
-    return this.httpClient.post(`http://localhost:8000/work?application_name=angular`, activity, {
+    return this.httpClient.post(`http://localhost:8000/work`, activity, {
       headers : new HttpHeaders().set("Content-Type","application/json")
     });
   }
